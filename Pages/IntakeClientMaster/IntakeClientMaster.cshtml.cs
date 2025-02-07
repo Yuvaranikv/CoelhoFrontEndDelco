500bf8b15cce008f677034764cc7fab22339c2df
Yuvaranikv <Yuvarani@YUVI-WORK-PC>
2025-02-07 15:07:33 +0530

    modal popup ellipsis icon

---
+++ Pages/IntakeClientMaster/IntakeClientMaster.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages.IntakeClientMaster
+{
+    public class IntakeClientMasterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
