60ff7f23c817211f86f6f5c6ee9c6f6aeb77f97f
Yuvaranikv <Yuvarani@YUVI-WORK-PC>
2025-02-19 14:36:35 +0530

    Added immediate health issues screen

---
+++ Pages/ImmediateHealthIssues.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages
+{
+    public class ImmediateHealthIssuesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
