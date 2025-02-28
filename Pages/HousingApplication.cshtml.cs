051210111d80ee04edcd4e2479ca24346f381519
Yuvaranikv <Yuvarani@YUVI-WORK-PC>
2025-02-28 13:30:02 +0530

    Added housing history and housing application

---
+++ Pages/HousingApplication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages
+{
+    public class HousingApplicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/HousingHistory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages
+{
+    public class HousingHistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
