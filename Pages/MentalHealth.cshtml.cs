403f907374372290a957fce6871c91b9ec2880bd
Yuvaranikv <Yuvarani@YUVI-WORK-PC>
2025-03-03 13:42:41 +0530

    Added mentalhealthdiagnosis screen

---
+++ Pages/MentalHealth.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages
+{
+    public class MentalHealthModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
