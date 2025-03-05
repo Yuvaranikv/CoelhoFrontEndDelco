fb272c411b3eb6ffc0bc195f0100578c0e449685
Yuvaranikv <Yuvarani@YUVI-WORK-PC>
2025-03-05 14:32:42 +0530

    Added financial assistance screen

---
+++ Pages/ClientDashboard/FinancialAssistance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TURN.Pages.ClientDashboard
+{
+    public class FinancialAssistanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
